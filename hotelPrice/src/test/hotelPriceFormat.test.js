8b626c715b07af1321f59253e12a8bc017a4cd15
Antonio Radolović <anradol@unipu.hr>
2021-06-11 13:48:59 +0200

    test corect

--- hotelPrice/src/test/hotelPriceFormat.test.js
+++ hotelPrice/src/test/hotelPriceFormat.test.js
@@ -19,7 +19,37 @@ describe('function that sorts prices', function() {
 
     })
 
-    
+    it("should return correct order",function(){
+
+         let data = [
+            {from: "2021-01-01", 
+             to: "2021-02-01", 
+             price: 48.25
+            },
+        
+            {from: "2020-04-02",
+             to: "2020-04-04",
+            price: 11.5
+            },
+        
+            {from: "2020-03-02",
+             to: "2020-05-15",
+             price: 42.0
+            },
+        
+            {from: "2020-02-02",
+             to: "2020-03-01",
+             price: 18.25
+            },
+            ]
+            expect(priceListFormatter(data)).to.eql(['11.5 : 2020-04-02 do 2020-04-04',
+                                                     '18.25 : 2020-02-02 do 2020-03-01', 
+                                                     '42 : 2020-03-02 do 2020-05-15', 
+                                                     '48.25 : 2021-01-01 do 2021-02-01'])
+
+    })
+
+
 
 
 })
